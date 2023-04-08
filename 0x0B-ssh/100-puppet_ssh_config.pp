7eba927d19f8f84eebe9aea3574dfa1d335c52ee
Bioyekeen <101011304+Bioyekeen@users.noreply.github.com>
2023-04-08 10:11:11 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
-# configuring ssh_config using Puppet
+# Seting up my client config file
+include stdlib
 
-file_line {'Append a line to /etc/ssh/ssh_config':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line {'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
   replace => true,
 }
